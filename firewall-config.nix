e4cb8cca80328ddb92356bde49aaa8fe13c93c48
AnonDev111 <147006860+AnonDev111@users.noreply.github.com>
2024-06-04 18:43:54 -0500

    Update firewall-config.nix

--- firewall-config.nix
+++ firewall-config.nix
@@ -7,7 +7,45 @@
   networking.firewall.allowedTCPPorts = [];
   networking.firewall.allowedUDPPorts = [];
 
-  # Enable openSnitch
-  services.opensnitch.enable = true;
-  
+
+  # Enable opensnitch firewall and opensnitch-ui needed to run it
+  services = {
+    opensnitch.enable = true;
+    opensnitch-ui.enable = true;
+  };
+
+  services.opensnitch = {
+    rules = {
+      systemd-timesyncd = {
+        name = "systemd-timesyncd";
+        enabled = true;
+        action = "allow";
+        duration = "always";
+
+        operator = {
+          type ="simple";
+          sensitive = false;
+          operand = "process.path";
+          data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-timesyncd";
+        };
+      };
+
+      systemd-resolved = {
+        name = "systemd-resolved";
+        enabled = true;
+        action = "allow";
+        duration = "always";
+
+        operator = {
+          type ="simple";
+          sensitive = false;
+          operand = "process.path";
+          data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-resolved";
+        };
+      };
+
+
+    };
+  };
+
 }
