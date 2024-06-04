bb606540b94a08735f4d1e4c1a4f3b04e6120ebd
AnonDev111 <147006860+AnonDev111@users.noreply.github.com>
2024-06-04 05:30:48 -0500

    Create package-configuration.nix

---
+++ package-configuration.nix
@@ -0,0 +1,33 @@
+{ pkgs, config, ... }
+{
+  environment.systemPackages = with pkgs; [
+    htop
+    neofetch
+    git
+    zsh
+    eww
+    dunst
+    lib
+    libnotify
+    swww
+    kitty
+    rofi-wayland
+    vim
+    wget
+    firefox
+    libreoffice
+    hyprland
+    clamav
+    rkhunter
+    mkpasswd
+    steam
+    protonup
+    wine
+    lutris
+    mangohud
+    flatpak
+  ];
+  nixpkgs. config = {
+    allowUnfree = true;
+  };
+}
